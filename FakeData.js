25aa820418d46caabccbfe75d4ab27848afd7534
Team-Itexphere <94818387+Team-Itexphere@users.noreply.github.com>
2022-08-19 18:15:41 +0530

    Happy Day ...

---
+++ FakeData.js
@@ -0,0 +1,39 @@
+const UserList = [
+    {
+        id:1,
+        name: "John",
+        username: "John",
+        age: 20,
+        nationality: "Canada"
+    },
+    {
+        id:2,
+        name: "AAAA",
+        username: "AAAA",
+        age: 18,
+        nationality: "Brazil"
+    },
+    {
+        id:3,
+        name: "BBBB",
+        username: "BBBBS",
+        age: 23,
+        nationality: "United States"
+    },
+    {
+        id:4,
+        name: "CCCC",
+        username: "CCCC",
+        age: 25,
+        nationality: "United States"
+    },
+    {
+        id:5,
+        name: "DDDD",
+        username: "DDDD",
+        age: 20,
+        nationality: "Canada"
+    },
+]
+
+module.exports = { UserList }
